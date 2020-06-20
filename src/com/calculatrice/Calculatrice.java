aaf4fb15b1b2cd6c32b18913564521f45ed8471c
MohamedHalim-A <mohamed12halim@gmail.com>
2020-06-20 07:08:45 +0100

    Begin to implement methode who create button

--- src/com/calculatrice/Calculatrice.java
+++ src/com/calculatrice/Calculatrice.java
@@ -1,4 +1,5 @@
 package com.calculatrice;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,6 +8,9 @@ import javax.swing.JPanel;
 public class Calculatrice extends JFrame {
 	private JPanel conteneur = new JPanel();
 	private JLabel ecran = new JLabel("OH");
+	private String[] table = {"0","1","2","3","4","5","6","7","8","9","C","+","-","*","/","="};
+	private String valeur="";
+	private JButton[] bouton;
 	
 	
 	public Calculatrice() {
@@ -15,9 +19,15 @@ public class Calculatrice extends JFrame {
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setContentPane(ecran);
-		
+		conteneur.add(ecran);
+		this.setContentPane(conteneur);
+		//implement();
 		this.setVisible(true);
+	}
+	public void implement() {
+		for (int i=0; i<table.length; i++) {
+			bouton[i]= new JButton(table[i]);
+		}
 	}
 
 }
